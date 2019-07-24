4a0c4cef99ad819df4964ee8f464544cf4aba31c
Circus Instructor <chris@creativecircus.edu>
2019-07-24 16:47:00 -0400

    ready for students?

---
+++ dist/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+console.log("Hello World from main.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+var CARD_COUNT = 10;
+var cardURLs = [];
+var gameBoardUL = document.querySelector('ul.game'); // URL for images.
+//https://loremflickr.com/200/300/cat?lock=1
+//# sourceMappingURL=main.js.map
---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+console.log(`Hello World from main.js! 
+Change this message, and make sure it changes in the browser 
+to verify that you're working in the right files.`)
+
+
+const CARD_COUNT = 10
+let cardURLs = []
+let gameBoardUL = document.querySelector('ul.game')
+
+// URL for images.
+//https://loremflickr.com/200/300/cat?lock=1
